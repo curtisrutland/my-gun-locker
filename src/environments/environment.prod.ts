f766e1715ea7a266b71686793331056fb3614880
Curtis Rutland <curtisrutland@gmail.com>
2018-07-22 23:20:32 -0500

    undid base environment, didn't work with prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
-import { baseEnvironment } from "./base";
+
 
 export const environment = {
-  production: true,
-  ...baseEnvironment
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyC0t2gakzQUsOnvLnL6RM1FwEfjVNgWY4w",
+    authDomain: "my-gun-locker.firebaseapp.com",
+    databaseURL: "https://my-gun-locker.firebaseio.com",
+    projectId: "my-gun-locker",
+    storageBucket: "my-gun-locker.appspot.com",
+    messagingSenderId: "42273997774"
+  },
+  version: "0.1.1",
+  projectLink: "https://github.com/curtisrutland/my-gun-locker"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,13 @@
-import { baseEnvironment } from "./base";
-
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-
-
 export const environment = {
   production: false,
-  ...baseEnvironment
+  firebase: {
+    apiKey: "AIzaSyC0t2gakzQUsOnvLnL6RM1FwEfjVNgWY4w",
+    authDomain: "my-gun-locker.firebaseapp.com",
+    databaseURL: "https://my-gun-locker.firebaseio.com",
+    projectId: "my-gun-locker",
+    storageBucket: "my-gun-locker.appspot.com",
+    messagingSenderId: "42273997774"
+  },
+  version: "0.1.1",
+  projectLink: "https://github.com/curtisrutland/my-gun-locker"
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
